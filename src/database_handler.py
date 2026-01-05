c3150fea0e0f26bb03199ffca09ee7e84e5321b0
Luis Hibert Santana <ahibertjr@gmail.com>
2026-01-04 22:48:00 -0600

    some bits and pieces

--- src/database_handler.py
+++ src/database_handler.py
@@ -1,14 +1,53 @@
-class Database:
+import logging
+
+from gremlin_python.process.anonymous_traversal import traversal
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+
+from models.words import WordNode, LexicalRelationshipEdge
+
+
+log = logging.getLogger(__name__)
+
+
+class Database:    
+
+    @staticmethod
     def get_connection(self):
-        pass
+        host = "ws://localhost:8182/gremlin"
+        g = traversal().with_remote(DriverRemoteConnection(host,'g'))
+        return g
 
 
 class DatabaseOperations:
+
     def __init__(self, word):
         self.word = word
+        self.g = Database.get_connection()
 
-    def retrieve_word(self):
-        pass
+    def get_word(self, word = None):
+        if word:
+            retrieved_word = self.g.V().has("vocabulary", "word", word).toList()
+        else:
+            retrieved_word = self.g.V().has("vocabulary", "word", self.word).toList()
+        return retrieved_word[0]
 
     def delete_word(self):
+        word_vertex = self.g.V().has("vocabulary", "word", self.word).next()
+        if word_vertex:
+            self.g.V(word_vertex).drop().iterate()
+            return f"{self.word} deleted from the dictionary"
+        else:
+            log.error(f"{self.word} does not exist in the dictionary")
+
+    def add_word(self, word_info: WordNode):
+        word_info_dict = word_info.model_dump()
+        traversal = self.g.add_v("vocabulary")
+        for key, value in word_info_dict:
+            traversal = traversal.property(key, value)
+        traversal.iterate()
+
+    def add_word_relationship(self, relationship: LexicalRelationshipEdge, word_2):
+        pass
+
+    def fetch_related_words():
         pass
--- src/words.py
+++ src/words.py
@@ -13,24 +13,38 @@ class Words:
 
     def create_word(self):
         """Creates a new word"""
-        pass
+        database = DatabaseOperations(self.word)
+        existing_word = database.get_word()
+        if existing_word:
+            log.error(f"{self.word} already exists in the dictionary")
+        else:
+            database.add_word()
+            return f"{self.word} added to the dictionary"
+
 
     def delete_word(self):
         """Deletes an existing word"""
         database = DatabaseOperations(self.word)
         try:
-            word_to_delete = DatabaseOperations.retrieve_word()
+            word_to_delete = DatabaseOperations.get_word()
         except Exception:
-            log.error(f"{self.word} is not in the dictionary")
+            log.error(f"{self.word} is not in the dictionary, cannot delete what is not there")
         database.delete_word
+        return f"{self.word} deleted from the dictionary."
 
-    def add_word_relationship(related_word: str, relationship: LexicalRelationships):
+    def add_word_relationship(self, related_word: str, relationship: LexicalRelationships):
         """Adds a lexical relationship to a word
         Args:
         - related_word: the word to add as a relationship
         - relationship: the type of relationship (synonym or antonym)
         """
-        pass
+        database = DatabaseOperations(self.word)
+        try:
+            database.get_word(related_word)
+        except Exception:
+            log.error(f"{related_word} does not exist in the dictionary. Cannot create relationship.")
+        database.add_word_relationship(related_word, relationship)
+        return f"{related_word} added as {relationship.relationship.value} to {self.word}"
 
     def retrieve_word(self) -> dict:
         """Retrieves general information about a given word"""
